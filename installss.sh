13f21f5906047f0052324cf687ac0eb73447363c
fankaljead <728678732@qq.com>
2018-04-08 13:27:40 +0000

    add ss install

--- installss.sh
+++ installss.sh
@@ -13,16 +13,16 @@ read ip_addr
 
 echo "Please set the password:"
 read ip_passwd
-echo '{
-	"server": "$ip_addr",
-	"server_port": 8388,
-	"local_address": "127.0.0.1",
-	"local_port": 1080,
-	"password": "$ip_passwd",
-	"timeout": 300,
-	"method": "aes-256-cfb",
-	"fast_open": false
-}'
+echo "{
+	\"server\": \"$ip_addr\",
+	\"server_port\": 8388,
+	\"local_address\": \"127.0.0.1\",
+	\"local_port\": 1080,
+	\"password\": \"$ip_passwd\",
+	\"timeout\": 300,
+	\"method\": \"aes-256-cfb\",
+	\"fast_open\": false
+}"> shadowsocks.json
 
 echo "starting sservice"
 
